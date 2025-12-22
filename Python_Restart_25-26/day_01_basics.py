40f3ea0869a863632916e1c2875b26e0472ae2a2
Harsh Nandal <harshnandal000@gmail.com>
2025-12-22 20:37:04 +0530

    Day 1: Python basics and input-output practice

---
+++ Python_Restart_25-26/day_01_basics.py
@@ -0,0 +1,31 @@
+name = "Harsh"
+age = 18
+college = "Jaat"
+
+print(name)
+print(age)
+print(college)
+
+print(type(name))
+print(type(age))
+print(type(college))
+
+print(age+5)
+print(name+ " Nandal")
+print(college+ " College Rohtak")
+
+# user input 
+User_name = input("Your Name please: ")
+User_age = input("Please enter your age: ")
+User_college = input("Please enter your college name: ")
+print("Hello ",User_name)
+user_age = int(User_age)
+print("Your age is ",5+user_age)
+print("Your college name is ",User_college)
+
+# what i learned in this python lesson:
+# Coding is fun
+# variables stores Data
+# input always gives str , and asks the user what to write.
+# int() can convert strings into integer type.
+# Errors are normal and make us learn faster.
